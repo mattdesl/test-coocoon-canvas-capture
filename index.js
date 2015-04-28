9eb59af1149fcf77ff66ef941b0bda54aaac9732
mattdesl <dave.des@gmail.com>
2015-04-28 00:07:05 -0400

    build

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require('./cocoon.js')
 
 require('touches')().on('start', function() {
   console.log("Sharing")
-  Cocoon.Social.share( 'Check out my sweet mustang! http://192.168.1.3:9966/pin.html' );
+  Cocoon.Social.share( 'Check out my sweet mustang! http://mattdesl.github.io/test-coocoon-canvas-capture/pin.html' );
 })
