7fb701afee136bd66678a505be97bfef0f840c78
mattdesl <dave.des@gmail.com>
2015-05-11 11:37:22 -0400

    testing bug

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 require('./cocoon.js')
 
-if (navigator.isCocoonJS) {
-  setTimeout(function() {
-    Cocoon.Dialog.showKeyboard({
-        type: Cocoon.Dialog.keyboardType.TEXT,
-    },{
-        insertText: function(inserted) {
-          console.log("Insert!", inserted)
-        },
-        deleteBackward: function() {
-          console.log("DELETE")
-        },
-        done: function() {
-          console.log("SUCCESS")
-        },
-        cancel: function(){
-          console.log("CANCEL")
-        }
-    });
-  }, 2000)
-}
+setTimeout(function() {
+  Cocoon.Dialog.showKeyboard({
+      type: Cocoon.Dialog.keyboardType.TEXT,
+  },{
+      insertText: function(inserted) {
+        console.log("Insert!", inserted)
+      },
+      deleteBackward: function() {
+        console.log("DELETE")
+      },
+      done: function() {
+        console.log("SUCCESS")
+      },
+      cancel: function(){
+        console.log("CANCEL")
+      }
+  });
+}, 2000)
