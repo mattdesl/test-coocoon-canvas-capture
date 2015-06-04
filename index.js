b5e5f40a5addb77555799113b92cbe93cbff34d1
mattdesl <dave.des@gmail.com>
2015-06-04 15:57:35 -0400

    testing threejs

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
-var COPY = 'كل سابق عقبت والكوري كان, و كرسي';
-var FONT_SIZE = 48;
-var WORDS = COPY.split(' ');
+var WORDS = ['كل سابق', 'عقبت والكوري', 'كان, و كرسي']
+// var WORDS = ['hello', 'dog', 'cat']
+var FONT_SIZE = 44
 
-document.body.style.margin = 0;
+document.body.style.margin = 0
 
+var wordContext = require('2d-context')()
+var wordCanvas = wordContext.canvas
 
-renderCanvas(WORDS);
-renderDiv(WORDS);
+var THREE = require('three')
+var createOrbitViewer = require('three-orbit-viewer')(THREE)
+var app = createOrbitViewer({
+  clearColor: 0x000000,
+  clearAlpha: 1.0,
+  fov: 65,
+  position: new THREE.Vector3(1, 1, -2)
+})
 
+var geo = new THREE.BoxGeometry(1, 1, 1)
+var tex = new THREE.Texture()
 
+var mat = new THREE.MeshBasicMaterial({ transparent: false, map: tex, color: 0xff0000 })
+var box = new THREE.Mesh(geo, mat)
+app.scene.add(box)
 
+var idx = 0
+render(idx)
+setInterval(function() {
+    idx ++
+    render(idx % WORDS.length)
+}, 50)
 
+function render (index) {
+  var word = WORDS[index]
+  wordContext.font = FONT_SIZE + 'px Arial'
+  // wordContext.font = FONT_SIZE + 'px Baghdad'
+  wordContext.fillStyle = '#FFF'
+  // var result = wordContext.measureText(word)
 
+  // var width = result.width
+  var div = 16
+  // wordCanvas.width = 300
+  // wordCanvas.height = 150
+  
+  wordContext.clearRect(0, 0, wordCanvas.width, wordCanvas.height)
+  wordContext.fillText(word, 5, FONT_SIZE)
 
-
-function renderDiv(words) {
-
-    var div = document.createElement('div');
-
-    div.style.fontFamily = 'Baghdad';
-    div.style.fontSize = FONT_SIZE + 'px';
-    div.style.position = 'absolute';
-    div.style.left = div.style.top = window.innerWidth * 0.5 + 'px';
-    div.style.top = '0px';
-    div.style.width = window.innerWidth * 0.5 + 'px';
-    div.style.height = window.innerHeight + 'px';
-    div.style.color = '#FFF';
-    div.style.background = '#000';
-    // div.style.height = FONT_SIZE + 'px';
-
-    document.body.appendChild(div);
-
-    words.forEach(function(word, idx) {
-        var wordDiv = document.createElement('div');
-        wordDiv.innerHTML = word;
-        
-        div.appendChild(wordDiv);
-    });
-}
-
-function renderCanvas(words) {
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
-
-    canvas.width = window.innerWidth * 0.5;
-    canvas.height = window.innerHeight;
-
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    document.body.appendChild(canvas);
-
-    words.forEach(function(word, idx) {
-    
-        context.font = FONT_SIZE + 'px Baghdad';
-        context.fillStyle = '#FFF';
-
-        context.fillText(word, 0, FONT_SIZE * idx + FONT_SIZE);
-    });
+  tex.image = wordCanvas
+  tex.needsUpdate = true
 }
