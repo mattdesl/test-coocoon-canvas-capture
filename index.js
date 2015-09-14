006273edd9abc17c37f5f0496bbd4e3f2f95fe93
mattdesl <dave.des@gmail.com>
2015-09-14 10:50:15 -0400

    cocoon

--- index.js
+++ index.js
@@ -1,110 +1,91 @@
-// This demo shows off custom text rendering and measuring.
-// It cycles through a few different examples.
+// var panorama = require('google-panorama-by-location')
+// var awesome = require('awesome-streetview')
+var equirect = require('google-panorama-equirectangular/intermediate')
+var THREE = require('three')
+var createOrbitViewer = require('three-orbit-viewer')(THREE)
+var bestZoom = require('google-panorama-zoom-level')
 
-var wrap = require('word-wrapper')
-
-// our font for the canvas
-var fontSize = 35
-var fontStyle = fontSize + 'px Baghdad, monospace'
-var lineHeight = fontSize * 0.9
-
-// some sample text to word-wrap
-var code = JSON.stringify(require('./package.json'), undefined, 2)
-// var lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, elit nec rutrum interdum, ipsum nulla auctor magna, ut tincidunt turpis ex efficitur orci. Aenean maximus interdum diam, nec consequat sapien ullamcorper ac. In et tortor dictum, commodo enim ac, sodales dolor.'
-var lipsum = 'ومضى مشاركة الإطلاق أم ذلك, فقد ما هنا؟ الحدود. ثم جهة اعلان اليابانية, قبل قامت للسيطرة الأوروبيّون هو. فقامت النفط الوراء ما بحق. الخاصّة المتساقطة، ما دار, بلا وبعد مساعدة من, قام بحشد حالية ما. أم تجهيز الأولى الساحة لها, نفس إذ مكّن الستار انتباه, بـ أضف ويعزى اليابان،. بل أعمال أدوات وانهاء بحق, بشكل الفرنسي جهة ٣٠.\n\nلم بحث اللا اعلان باستخدام, بتخصيص اللازمة لمّ ما, ذلك العسكري الأهداف أم. ترتيب وعُرفت الإحتفاظ أخر عن, تم ضرب تجهيز لهيمنة الإقتصادي. الحكم وبحلول ٣٠ إيو. الا وبحلول تكتيكاً لم. اسبوعين واقتصار اليابانية ان وتم. يبق طوكيو يونيو الأهداف أم, الأخذ أوروبا الموسوعة بها ٣٠.\n\n٣٠ وفي فكانت الشمل مساعدة. الشتاء، الفرنسي الخارجية أي يبق, ٣٠ وفي جنوب المدن التغييرات. أم لإعادة ليرتفع دول. وعلى إختار الأوروبية بها لم. ان ونتج الرئيسية إيو. وصغار ديسمبر الإحتفاظ هو حدى, بـ بعض كُلفة وحلفاؤها.'
-var multiline = 'ميكو هو إنسان الرهيب الذي تفوح منها رائحة. انه يحتاج الى الاستحمام أكثر وارتداء ملابس نظيفة. الحصن هي في الواقع تبريد السيارات التي أدلى بها شركة فورد للسيارات من ديترويت ميشيغان.'
-
-// create a canvas2d
-var context = require('2d-context')({
-  width: window.innerWidth,
-  height: window.innerHeight
+var app = createOrbitViewer({
+  clearColor: 0xffffff,
+  clearAlpha: 1.0,
+  fov: 70,
+  position: new THREE.Vector3(0, 0, -0.1)
 })
 
-// setup font metrics
-var metrics = createMetrics(context, fontStyle)
+var tex = new THREE.DataTexture(null, 1, 1, THREE.RGBAFormat)
+tex.minFilter = THREE.LinearFilter
+tex.magFilter = THREE.LinearFilter
+tex.generateMipmaps = false
 
-// some examples to see different modes and text
-var slides = [ {
-  text: lipsum, description: 'normal at 200px',
-  width: 800,
-  measure: metrics
-}, {
-  text: multiline, description: 'nowrap (newline only)',
-  mode: 'nowrap',
-  measure: metrics
-}, {
-  text: code, description: 'pre',
-  mode: 'pre'
-}, {
-  text: code, description: 'pre clipped to 200px',
-  mode: 'pre',
-  width: window.innerWidth,
-  measure: metrics
-} ]
+// add a double-sided sphere
+var geo = new THREE.SphereGeometry(1, 84, 84)
+var mat = new THREE.MeshBasicMaterial({
+  map: tex,
+  side: THREE.DoubleSide
+})
+var sphere = new THREE.Mesh(geo, mat)
+sphere.scale.x = -1
 
-// This will wordwrap and draw the example "slide" 
-function draw (context, opt) {
-  var text = opt.text
+// this looks pretty nice with our data set
+sphere.rotation.y = Math.PI/2 - 0.5
 
-  // wrap the text and get back a list of lines
-  var lines = wrap.lines(text, opt)
+app.scene.add(sphere)
 
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-  context.save()
-  context.translate(20, 20)
+render({
+  id: 'ZI4ZNb4CDCEUBpGNJRSkaw',
+  worldSize: { width: 13312, height: 6656 },
+  tileSize: { width: 512, height: 512 }
+})
 
-  // now draw each line
-  context.font = fontStyle
-  context.fillStyle = 'white'
-  var lineWidths = lines.map(function (line) {
-    var str = text.substring(line.start, line.end)
-    return context.measureText(str).width
+// function load () {
+//   var location = awesome()
+//   sphere.visible = false
+//   console.log('Location: %s', location.join(', '))
+//   panorama(location, function (err, result) {
+//     if (err) throw err
+//     console.log(result.id)
+//     console.log(result.tiles.worldSize, result.tiles.tileSize)
+//   })
+// }
+
+function render (opt) {
+  var renderer = app.renderer
+
+  // ensure handle is created
+  renderer.uploadTexture(tex)
+
+  var gl = renderer.getContext()
+  var handle = tex.__webglTexture
+  var texHeight
+  var maxSize = gl.getParameter(gl.MAX_TEXTURE_SIZE)
+  var zoom = Math.max(0, Math.min(4, bestZoom(maxSize)))
+    
+  equirect(opt.id, {
+    zoom: zoom,
+    tiles: {
+      worldSize: opt.worldSize,
+      tileSize: opt.tileSize
+    },
+    crossOrigin: 'Anonymous'
+  }).on('start', function (data) {
+    texHeight = data.height
+
+    // reshape the texture initially with transparent data
+    gl.bindTexture(gl.TEXTURE_2D, handle)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, data.width, data.height,
+          0, gl.RGBA, gl.UNSIGNED_BYTE, null)
+
+    // now we can show the sphere
+    sphere.visible = true
+  }).on('progress', function (ev) {
+    var x = ev.position[0]
+    var y = texHeight - ev.position[1] - ev.image.height
+
+    // now blit the intermediate image
+    gl.bindTexture(gl.TEXTURE_2D, handle)
+    gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y,
+          gl.RGBA, gl.UNSIGNED_BYTE, ev.image)
+  }).on('complete', function () {
+    console.log('done loading')
   })
-
-  var maxLineWidth = lineWidths.reduce(function (prev, b) {
-    return Math.max(prev, b)
-  }, 0)
-
-  lines.forEach(function (line, i) {
-    var str = text.substring(line.start, line.end)
-    var lineWidth = lineWidths[i]
-    context.fillText(str, (maxLineWidth - lineWidth), 10 + i * lineHeight)
-  })
-
-  context.restore()
-}
-
-function createMetrics (context, font) {
-  context.font = font
-  var charWidth = context.measureText('ن').width
-  console.log('CHAR WIDTHa', charWidth)
-  return function measure (text, start, end, width) {
-    // measures the chunk of text, returning the substring
-    // we can fit within the given width
-    var availableGlyphs = Math.floor(width / charWidth)
-    var totalGlyphs = Math.floor((end - start) * charWidth)
-    var glyphs = Math.min((end - start), availableGlyphs, totalGlyphs)
-    return {
-      start: start,
-      end: start + glyphs
-    }
-  }
-}
-
-// once the dom is ready, load up our app
-document.body.appendChild(context.canvas)
-document.body.style.background = 'black'
-var index = 0
-
-// start app
-change(slides[index])
-
-// cycle through modes
-// setInterval(function() {
-//     index++
-//     change(slides[index % slides.length])
-// }, 2500)
-
-function change (slide) {
-  draw(context, slide)
 }
